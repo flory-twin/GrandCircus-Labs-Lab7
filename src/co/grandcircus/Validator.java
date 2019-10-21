d07d7f8049126b1b81177a868d0e2bcd3aa0b015
flory-twin <flory.twin.2@gmail.com>
2019-10-21 16:41:13 -0400

    Initial commit.

---
+++ src/co/grandcircus/Validator.java
@@ -0,0 +1,24 @@
+package co.grandcircus;
+
+public class Validator
+{
+   public boolean isNameValid(String name)
+   {
+      return false;
+   }
+   
+   public boolean isEmailValid(String email)
+   {
+      return false;
+   }
+   
+   public boolean isPhoneNumberValid(String phoneNumber)
+   {
+      return false;
+   }
+   
+   public boolean isDateValid(String date)
+   {
+      return false;
+   }
+}
---
+++ src/co/grandcircus/test/ValidatorTest.java
@@ -0,0 +1,37 @@
+package co.grandcircus.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest
+{
+   
+   @Test
+   public void testIsNameValid()
+   {
+      //isNameValid(String name);
+      fail("Not yet implemented.");
+   }
+   
+   @Test
+   public void testIsEmailValid()
+   {
+      //isEmailValid(String email)
+      fail("Not yet implemented.");
+   }
+   
+   @Test
+   public void testIsPhoneNumberValid() {
+      //isPhoneNumberValid(String phoneNumber)
+      fail("Not yet implemented.");
+      
+   }
+   
+   @Test
+   public void testIsDateValid()
+   {
+      //isDateValid(String date)
+      fail("Not yet implemented.");
+   }
+}
